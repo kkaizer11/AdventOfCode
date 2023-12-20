41f21c38354499995d13fab7327240a670c8fa58
kkaizer11 <kaizerin.m@gmail.com>
2023-12-20 20:56:22 -0300

    tentando fazer o read funcionar

--- rust/aoc-2023/src/day1/aux.rs
+++ rust/aoc-2023/src/day1/aux.rs
@@ -1,3 +1,14 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 pub fn diga_oi() {
     println!("Dizendo oi");
+}
+
+pub fn read() -> std::io::Result<()> {
+    let mut file = File::open("day1/input.txt")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    assert_eq!(contents, "Hello, world!");
+    Ok(())
 }
--- rust/aoc-2023/src/main.rs
+++ rust/aoc-2023/src/main.rs
@@ -1,6 +1,9 @@
 use crate::day1::aux::diga_oi;
+use crate::day1::aux::read;
+
 pub mod day1;
 
 fn main() {
     diga_oi();
+    read();
 }
